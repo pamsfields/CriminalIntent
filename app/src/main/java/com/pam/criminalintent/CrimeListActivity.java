7c3aef1cd01777ffa658409d08b3d956aedf3109
pamsfields <jh5811zj@go.minneapolis.edu>
2017-10-14 16:40:05 -0500

    updated project with recyclerview

---
+++ app/src/main/java/com/pam/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.pam.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Pam on 10/14/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeListFragment();
+    }
+}
